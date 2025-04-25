3211d713e788bf3abc24e76f80e5ea70bd75f0d1
Azzahra Rinaldi <zaza@Azzahras-Macbook.local>
2025-04-25 09:27:52 +0700

    fixing header home-admin

--- lib/ui/admin-front/home-admin/components/header.dart
+++ lib/ui/admin-front/home-admin/components/header.dart
@@ -1,3 +1,4 @@
+import 'package:bersihku/const.dart';
 import 'package:flutter/material.dart';
 
 class HeaderSection extends StatelessWidget {
@@ -7,27 +8,52 @@ class HeaderSection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.05, vertical: 20),
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-        children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: const [
-              Text(
-                "Selamat Datang,",
-                style: TextStyle(fontSize: 18, color: Colors.white),
+    return Column(
+      children: [
+        Container(
+          padding: EdgeInsets.symmetric(
+              horizontal: screenWidth * 0.05, vertical: 15),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: const [
+                  Text(
+                    "Selamat Datang,",
+                    style: TextStyle(fontSize: 18, color: Colors.white),
+                  ),
+                  Text(
+                    "Nuara 👋🏻",
+                    style: TextStyle(
+                        fontSize: 28,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white),
+                  ),
+                ],
               ),
-              Text(
-                "Nuara 👋🏻",
-                style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold, color: Colors.white),
-              ),
+              Image.asset("assets/icons/notification.png",
+                  width: 40, height: 40),
             ],
           ),
-          Image.asset("assets/icons/notification.png", width: 40, height: 40),
-        ],
-      ),
+        ),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: screenWidth * 0.05),
+          child: Container(
+            alignment: Alignment.centerLeft,
+            padding: const EdgeInsets.all(8),
+            decoration: BoxDecoration(
+              color: Colors.white.withOpacity(0.6),
+            ),
+            child: Text(
+              "Riwayat Pengangkutan Terbaru",
+              style:
+                  TextStyle(color: textPrimary, fontWeight: FontWeight.bold),
+            ),
+          ),
+        ),
+        SizedBox(height: 20)
+      ],
     );
   }
 }
